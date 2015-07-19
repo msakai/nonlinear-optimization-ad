c3ab315eb2950895f2356dbc12ea488ace9e6cd4
Masahiro Sakai <masahiro.sakai@gmail.com>
2015-07-19 15:05:21 +0900

    clean up samples/LinearRegression.hs

--- samples/LinearRegression.hs
+++ samples/LinearRegression.hs
@@ -1,7 +1,6 @@
+{-# OPTIONS_GHC -Wall #-}
 module Main where
 
-import Control.Monad
-import qualified Data.Vector as V
 import qualified Numeric.Optimization.Algorithms.HagerZhang05.AD as CG
 import Text.Printf
 import qualified Text.CSV as CSV
@@ -17,9 +16,20 @@ main = do
       cost theta = mse [(CG.auto x, CG.auto y) | (x,y) <- samples] (h theta)
       params   = CG.defaultParameters{ CG.printFinal = True, CG.printParams = True, CG.verbose = CG.Verbose }
       grad_tol = 0.0000001
-  (theta, result, stat) <- CG.optimize params grad_tol [0,0] cost
-  print theta
+  (theta@[theta0,theta1], _result, _stat) <- CG.optimize params grad_tol [0,0] cost
+  printf "y = %f x + %f\n" theta1 theta0
+  printf "MSE = %f\n" (mse samples (h theta))
+  printf "R^2 = %f\n" (r2 samples (h theta))
 
 -- mean squared error
 mse :: Fractional y => [(x,y)] -> (x -> y) -> y
-mse samples h = sum [(h x - y)^(2::Int) | (x,y) <- samples] / fromIntegral (length samples)
+mse samples h = mean [(h x - y)^(2::Int) | (x,y) <- samples]
+
+r2 :: Fractional y => [(x,y)] -> (x -> y) -> y
+r2 samples h = 1 - mse samples h / sum [(y - ym)^(2::Int) | y <- ys]
+  where
+    ys = map snd samples
+    ym = mean ys
+
+mean :: Fractional x => [x] -> x
+mean xs = sum [x | x <- xs] / fromIntegral (length xs)
