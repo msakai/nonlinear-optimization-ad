51beb8876eab1bf262c2fe0e36160dc3c8e82dd2
Masahiro Sakai <masahiro.sakai@gmail.com>
2023-06-28 12:09:30 +0900

    fix outdated example code in haddock

--- numeric-optimization-ad/src/Numeric/Optimization/AD.hs
+++ numeric-optimization-ad/src/Numeric/Optimization/AD.hs
@@ -113,7 +113,7 @@ instance Opt.Optionally (Opt.HasHessian (ProblemReverse f)) where
 -- > 
 -- > main :: IO ()
 -- > main = do
--- >   (x, result, stat) <- minimizeReverse LBFGS def rosenbrock Nothing [] [-3,-4]
+-- >   result <- minimize LBFGS def rosenbrock Nothing [] [-3,-4]
 -- >   print (resultSuccess result)  -- True
 -- >   print (resultSolution result)  -- [0.999999999009131,0.9999999981094296]
 -- >   print (resultValue result)  -- 1.8129771632403013e-18
